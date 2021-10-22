1cc6c15229f4ccf4ec8ea971213bb6c042888f91
derek <harrison.derek.w@gmail.com>
2021-10-22 18:40:48 +0200

    Initial Commit

---
+++ matrix-mult/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  matrix-mult
+//
+//  Created by Derek Harrison on 22/10/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
