fd839cca5230937ff010ee2ef6e240bac8cd4c4f
derek <harrison.derek.w@gmail.com>
2021-10-23 17:39:14 +0200

    also return matrix product solution

--- matrix-mult/main.cpp
+++ matrix-mult/main.cpp
@@ -7,15 +7,24 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <time.h>
 
 const int inf = 3e+8;
 
 typedef struct memo_table_element {
     bool is_set;
     int num_ops;
+    int cut;
 } m_table_elem;
 
 typedef m_table_elem** m_table;
+
+typedef struct parenthesis_element {
+    int num_l_par;
+    int num_r_par;
+    int num_l_par_pos;
+    int num_r_par_pre;
+} p_elem;
 
 m_table new_memo_table(int n, int m) {
     m_table r = new m_table_elem*[n];
@@ -35,6 +44,88 @@ void free_memo_table(m_table r, int n) {
     delete [] r;
 }
 
+void make_par_tree(int i, int j, m_table memo_table, p_elem* par_tree) {
+    
+    if(j - i > 2) {
+        int min_k = memo_table[i][j].cut;
+    
+        make_par_tree(i, min_k, memo_table, par_tree);
+        make_par_tree(min_k, j, memo_table, par_tree);
+
+        if(min_k - i > 1) {
+            par_tree[i].num_l_par_pos++;
+            par_tree[min_k].num_r_par_pre++;
+        }
+        
+        if(j - min_k > 1) {
+            par_tree[min_k].num_l_par_pos++;
+            par_tree[j].num_r_par_pre++;
+        }
+    }
+}
+
+int check_num_ops(int i, int j, int p[], m_table memo_table) {
+    int num_of_ops = 0;
+    
+    if(j - i == 2) {
+        num_of_ops = p[i] * p[i+1] * p[j];
+    }
+    
+    if(j - i > 2) {
+        int min_k = memo_table[i][j].cut;
+        int num_ops1 = check_num_ops(i, min_k, p, memo_table);
+        int num_ops2 = check_num_ops(min_k, j, p, memo_table);
+        
+        num_of_ops = num_ops1 + num_ops2 + p[i] * p[min_k] * p[j];;
+    }
+    
+    return num_of_ops;
+}
+
+int verify_num_ops(int n, int p[], m_table memo_table) {
+    
+    //Verify number of operations by computing matrix product
+    int num_ops = check_num_ops(0, n - 1, p, memo_table);
+    
+    return num_ops;
+}
+
+void print_tree(int i, int j, m_table memo_table) {
+    
+    //Initialize parenthesis tree
+    int n = j - i + 1;
+    p_elem* par_tree = new p_elem[n];
+    for(int it = 0; it < n; ++it) {
+        par_tree[it].num_l_par = 0;
+        par_tree[it].num_r_par = 0;
+        par_tree[it].num_l_par_pos = 0;
+        par_tree[it].num_r_par_pre = 0;
+    }
+    
+    //Make parenthesis tree
+    make_par_tree(i, j, memo_table, par_tree);
+    
+    //Print tree
+    std::cout << "printing matrix product solution:" << std::endl;
+    for(int it = 0; it < n; ++it) {
+        int num_l_par = par_tree[it].num_l_par;
+        int num_r_par = par_tree[it].num_r_par;
+        int num_l_par_pos = par_tree[it].num_l_par_pos;
+        int num_r_par_pre = par_tree[it].num_r_par_pre;
+        
+        for(int it = 0; it < num_r_par_pre; ++it) { std::cout << ")"; }
+        for(int it = 0; it < num_l_par_pos; ++it) { std::cout << "("; }
+        for(int it = 0; it < num_l_par; ++it) { std::cout << "("; }
+        if(it < n - 1) { std::cout << "A" << it + 1; }
+        for(int it = 0; it < num_r_par; ++it) { std::cout << ")"; }
+    }
+    
+    std::cout << std::endl;
+    
+    //Free data
+    delete [] par_tree;
+}
+
 int min_ops(int p[], int i, int j, m_table memo_table) {
     int min_nops = inf;
     
@@ -48,6 +139,7 @@ int min_ops(int p[], int i, int j, m_table memo_table) {
     if(j - i <= 1) {
         memo_table[i][j].is_set = true;
         memo_table[i][j].num_ops = 0;
+        memo_table[i][j].cut = 0;
         return 0;
     }
     
@@ -56,6 +148,7 @@ int min_ops(int p[], int i, int j, m_table memo_table) {
         min_nops = p[i] * p[i+1] * p[j];
         memo_table[i][j].is_set = true;
         memo_table[i][j].num_ops = min_nops;
+        memo_table[i][j].cut = 0;
         return min_nops;
     }
     
@@ -78,15 +171,21 @@ int min_ops(int p[], int i, int j, m_table memo_table) {
         //Set memo table
         memo_table[i][j].is_set = true;
         memo_table[i][j].num_ops = min_nops;
+        memo_table[i][j].cut = min_k;
     }
     
     return min_nops;
 }
 
-int minimum_num_ops(int p[], int n) {
+void print_solution(int n, m_table memo_table) {
+    
+    //Print tree
+    print_tree(0, n - 1, memo_table);
+}
+
+int minimum_num_ops(int p[], int n, m_table memo_table) {
     
     //Initialize memo table
-    m_table memo_table = new_memo_table(n, n);
     for(int i = 0; i < n; ++i) {
         for(int j = 0; j < n; ++j) {
             memo_table[i][j].is_set = false;
@@ -97,31 +196,39 @@ int minimum_num_ops(int p[], int n) {
     //Compute minimum number of operations
     int min_num_ops = min_ops(p, 0, n - 1, memo_table);
     
-    //Free data
-    free_memo_table(memo_table, n);
-    
     return min_num_ops;
 }
 
 int main(int argc, const char * argv[]) {
     
     //Input size
-    int n = 15;
+    int n = 8;
+    
+    //Allocate space for results
+    m_table memo_table = new_memo_table(n, n);
     
     //Creating random sequence of matrix dimensions
     int* p = new int[n];
+    srand((unsigned) time(NULL));
     for(int i = 0; i < n; ++i) {
         p[i] = rand() % n + 1;
     }
     
     //Compute minimum number of operations
-    int min_num_ops = minimum_num_ops(p, n);
+    int min_num_ops = minimum_num_ops(p, n, memo_table);
+
+    //Verify number of operations
+    int min_num_ops_ref = verify_num_ops(n, p, memo_table);
+    
+    //Print results
+    print_solution(n, memo_table);
+    std::cout << "min_num_ops: " << min_num_ops << std::endl;
+    std::cout << "min_num_ops_ref: " << min_num_ops_ref << std::endl;
     
     //Free data
     delete [] p;
+    free_memo_table(memo_table, n);
     
-    //Print results
-    std::cout << "min_num_ops: " << min_num_ops << std::endl;
     std::cout << "done" << std::endl;
     
     return 0;
